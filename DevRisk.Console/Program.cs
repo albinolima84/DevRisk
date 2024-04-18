a43451a03280a6acd8ca8ab692fb56473358ee54
Albino Lima <albinolima@protonmail.com>
2024-04-18 08:46:36 -0300

    Adiciona leitura ao arquivo e tratamentos de erros

--- DevRisk.Console/Program.cs
+++ DevRisk.Console/Program.cs
@@ -1,13 +1,16 @@
 ﻿using DevRisk.Domain;
-using System.IO;
+using System.Globalization;
+
 internal class Program
 {
     private static void Main(string[] args)
     {
+        var culture = CultureInfo.GetCultureInfo("en-US");
         string path = Directory.GetCurrentDirectory();
         StreamReader content = new($@"{path}/input.txt");
+        StreamWriter output = new($@"{path}/output.txt");
         var referenceDateFromFile = content.ReadLine();
-        if (!DateTime.TryParse(referenceDateFromFile, out DateTime referenceDate))
+        if (!DateTime.TryParse(referenceDateFromFile, culture, out DateTime referenceDate))
         {
             Console.WriteLine($"Incorrect file format.");
             Environment.Exit(0);
@@ -17,9 +20,30 @@ internal class Program
         var trade = content.ReadLine();
         while (trade != null)
         {
-        // var trade = new TradeDto(DateTime.Now.AddDays(11), "Public", 1_000_001);
-        // var instance = TradeFactory.Create(trade, DateTime.Now);
-        // Console.WriteLine(instance.Category);
+            var currentTrade = trade.Split(' ');
+            if(!DateTime.TryParse(currentTrade[2], culture, out DateTime nextPayment)) 
+            {
+                Console.WriteLine($"Error on processing line {trade}. Date format invalid");
+                trade = content.ReadLine();
+                continue;
+            }
+            var tradeDto = new TradeDto(nextPayment, currentTrade[1], double.Parse(currentTrade[0]));
+            try
+            {
+                var instance = TradeFactory.Create(tradeDto, referenceDate);
+                output.WriteLine(instance.Category);    
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error on generating Trade -> {ex.Message}");
+                continue;
+            }
+            finally 
+            {
+                trade = content.ReadLine();
+            }
         }
+        content.Close();
+        output.Close();
     }
 }
